ba7a4677a47f2fab6c1a0b79a6596a9e2cfc4e3f
tay3434 <tns5371@psu.edu>
2024-09-26 13:15:58 -0400

    homework 5 changes

--- src/my-card.js
+++ src/my-card.js
@@ -18,6 +18,7 @@ export class MyCard extends LitElement {
     this.description = "Default description";
     this.imageSrc = null;
     this.link = "#";
+    this.fancy = false;
   }
 
   static get styles() {
@@ -30,6 +31,13 @@ export class MyCard extends LitElement {
         box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
         overflow: hidden;
         width: 300px;
+        background-color: var(--my-card-fancy-bg, white);
+      }
+      :host([fancy]) {
+      display: block;
+        background-color: #2D3142;
+        border: 2px solid white;
+        box-shadow: 10px 5px 5px #4F5D75;
       }
     
 
@@ -73,9 +81,10 @@ export class MyCard extends LitElement {
     padding: 8px;
     margin: 8px 16px;
   }
-  .button:hover {
-          background-color: #0056b3;
-        }
+  
+  my-card[curly] {
+  --my-card-curly-bg:#b49ec7; ;
+}
 
     @media screen and (min-width: 500px) and (max-width: 800px) {
       .size {
@@ -89,8 +98,39 @@ export class MyCard extends LitElement {
         display: none;
       }
     }
+    details summary {
+    text-align: left;
+    font-size: 20px;
+    padding: 8px 0;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+
+  details[open] summary {
+    font-weight: bold;
+  
+  }
+  
+  details div {
+    border: 2px solid white;
+    border-radius: 10px;
+    text-align: left;
+    padding: 8px;
+    height: 70px;
+    overflow: auto;
+  }
     `;
   }
+    // put this anywhere on the MyCard class; just above render() is probably good
+    openChanged(e) {
+      console.log(e.newState);
+      if (e.newState === "open") {
+        this.fancy = true;
+      }
+      else {
+        this.fancy = false;
+      }
+    }
+
 
   render() {
     return html`
@@ -98,7 +138,12 @@ export class MyCard extends LitElement {
       <div class="card">
         <img class="img" src="${this.imageSrc}" alt="${this.title}">
         <h1>${this.title}</h1>
-        <p>${this.description}</p>
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+        <summary>Description</summary>
+          <div>
+          <slot>${this.description}</slot>
+        </div>
+        </details>
         <a href="${this.link}" target="_blank">
           <button class="button">Details</button>
         </a>
@@ -112,10 +157,44 @@ export class MyCard extends LitElement {
       title: { type: String },
       description: { type: String },
       imageSrc: { type: String },
-      link: { type: String }
+      link: { type: String },
+      fancy: { type: Boolean, reflect: true }
     };
   }
 }
+// document.querySelector('#duplicate').addEventListener('click', function(event) {
+//   if(document.querySelectorAll('card').length < 10){
+//      const newCard = document.querySelector('#container .card').cloneNode(true);
+//     document.querySelector('#container').appendChild(newCard);
+//   }
+// });
+
+// document.querySelector('#change-title').addEventListener('click', function(event){
+//   document.querySelector('.card-title').innerHTML = "College of IST";
+// });
+
+// document.querySelector('#change-image').addEventListener('click', function(event){
+//   document.querySelector('.card-image').src = "https://ist.psu.edu/sites/default/files/news/Westgate_windows_0.jpg";
+// });
+
+// document.querySelector('#bg-change').addEventListener('click', function(event) {
+//   const cards = document.querySelectorAll('.card');
+  
+//   cards.forEach(card => {
+//     if (card.classList.contains('fancy')) {
+//       card.classList.remove('fancy');
+//     } else {
+//       card.classList.add('fancy');
+//     }
+//   });
+// });
+
+// document.querySelector('#delete-card').addEventListener('click', function(event){
+//    if(document.querySelectorAll('.card').length > 1){
+//     document.querySelector('.card:last-child').remove();
+//   }
+// });
+
 
 
 globalThis.customElements.define(MyCard.tag, MyCard);
